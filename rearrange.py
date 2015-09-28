8e8e9afe7b3e3d758a583df3b7d9fe43a446d7fa
vikisimovska <violeta.simovska@students.makeschool.com>
2015-09-27 20:37:26 -0700

    Rearrange exercise completed.

---
+++ rearrange.py
@@ -0,0 +1,16 @@
+import random
+import sys
+
+
+arguments = sys.argv
+words = arguments[1:]
+
+newRanomizedArray = []
+
+while len(words) > 0:
+    tempRandomPosition = random.randint(0, len(words) - 1)
+    newRanomizedArray.append(words[tempRandomPosition])
+    del words[tempRandomPosition]
+    pass
+
+print(newRanomizedArray)
